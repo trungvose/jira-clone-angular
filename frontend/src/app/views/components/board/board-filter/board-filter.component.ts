d4c0e7adab94986940575433635e23e1c64ca812
Trung Vo <trung.vo@zyllem.com>
2020-06-16 23:25:32 +0800

    Add until destroy and update the search

--- frontend/src/app/views/components/board/board-filter/board-filter.component.ts
+++ frontend/src/app/views/components/board/board-filter/board-filter.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { JFilter } from 'src/app/interface/filter';
-
+import { untilDestroyed, UntilDestroy } from '@ngneat/until-destroy';
 @Component({
   selector: 'board-filter',
   templateUrl: './board-filter.component.html',
   styleUrls: ['./board-filter.component.scss']
 })
+@UntilDestroy()
 export class BoardFilterComponent implements OnInit {
   filter: JFilter;
+  searchControl: FormControl = new FormControl();
   @Output() filterChanged = new EventEmitter();
   constructor() {
     this.filter = new JFilter();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(300), distinctUntilChanged(), untilDestroyed(this))
+      .subscribe((term) => {
+        this.filter.searchTerm = term;
+      });
+  }
 
   recentUpdateChanged() {
     this.filter.recentUpdate = !this.filter.recentUpdate;
