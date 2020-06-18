0df4a711423d4dbbcff4a92c947ab8a13ddc2886
Trung Vo <trung.vo@zyllem.com>
2020-06-18 22:26:01 +0800

    Add filter function

--- frontend/src/app/project/components/board/board-dnd-list/board-dnd-list.component.ts
+++ frontend/src/app/project/components/board/board-dnd-list/board-dnd-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { IssueStatusDisplay, IssueStatus, JIssue } from '@trungk18/interface/issue';
+import { FilterState } from '@trungk18/project/state/filter/filter.store';
 
 @Component({
   selector: '[board-dnd-list]',
@@ -10,6 +11,7 @@ import { IssueStatusDisplay, IssueStatus, JIssue } from '@trungk18/interface/iss
 export class BoardDndListComponent implements OnInit {
   IssueStatusDisplay = IssueStatusDisplay;
   @Input() status: IssueStatus;
+  @Input() currentUserId: string;
   @Input() issues: JIssue[];
   get issuesCount(): number {
     return 0;
@@ -21,5 +23,23 @@ export class BoardDndListComponent implements OnInit {
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray([], event.previousIndex, event.currentIndex);
+  }
+  
+  filterIssues(issues: JIssue[], filter: FilterState): JIssue[] {
+    const { onlyMyIssue, recentUpdate, searchTerm, userIds } = filter;
+    return issues.filter((issue) => {
+      let isMatchTerm = searchTerm
+        ? issue.title.toLowerCase().includes(searchTerm.trim().toLowerCase())
+        : true;
+
+      let isIncludeUsers = userIds.length
+        ? issue.userIds.some((userId) => userIds.includes(userId))
+        : true;
+      let isMyIssue = onlyMyIssue
+        ? this.currentUserId && issue.userIds.includes(this.currentUserId)
+        : true;
+
+      return isMatchTerm && isIncludeUsers && isMyIssue;
+    });
   }
 }
