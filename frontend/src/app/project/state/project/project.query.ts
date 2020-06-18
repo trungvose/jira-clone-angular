daabf5b24e6c9165d95a8a886af4afc0669ee7cd
Trung Vo <trung.vo@zyllem.com>
2020-06-18 21:39:09 +0800

    Filter issues by status

--- frontend/src/app/project/state/project/project.query.ts
+++ frontend/src/app/project/state/project/project.query.ts
@@ -1,6 +1,9 @@
 import { ProjectState, ProjectStore } from './project.store';
 import { Injectable } from '@angular/core';
 import { Query } from '@datorama/akita';
+import { IssueStatus, JIssue } from '@trungk18/interface/issue';
+import { filter, map } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,5 +14,14 @@ export class ProjectQuery extends Query<ProjectState> {
   isLoading$ = this.selectLoading();
   all$ = this.select();
   issue$ = this.select('issues');
+  
+  issueByStatus$ = (status: IssueStatus): Observable<JIssue[]> => {
+    return this.select('issues').pipe(
+      map((issues) => {
+        let filteredIssues = issues.filter((x) => x.status === status);
+        return filteredIssues;
+      })
+    );
+  };
   users$ = this.select('users');
 }
