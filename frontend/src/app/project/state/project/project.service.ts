82a4a6232b757de8026a7addfd3f5f540fbf4cc0
Trung Vo <trung.vo@zyllem.com>
2020-06-19 09:01:27 +0800

    Drag and drop workkkkkkkk

--- frontend/src/app/project/state/project/project.service.ts
+++ frontend/src/app/project/state/project/project.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { arrayUpdate } from '@datorama/akita';
+import { JIssue } from '@trungk18/interface/issue';
 import { JProject } from '@trungk18/interface/project';
 import { JUser } from '@trungk18/interface/user';
+import { Observable, of } from 'rxjs';
+import { catchError, finalize, map } from 'rxjs/operators';
 import { ProjectStore } from './project.store';
-import { finalize, catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,25 +17,36 @@ export class ProjectService {
   getProject(): Observable<JProject> {
     this._store.setLoading(true);
     return this._http.get<JProject>('/data/project.json').pipe(
-      map(project => {
-        this._store.update(state => {
+      map((project) => {
+        this._store.update((state) => {
           return {
             ...state,
             ...project
-          } 
-        })
+          };
+        });
       }),
       finalize(() => {
-        this._store.setLoading(false)
+        this._store.setLoading(false);
       }),
-      catchError(error => {
+      catchError((error) => {
         this._store.setError(error);
-        return of(error)
+        return of(error);
       })
     );
   }
 
   login(): Observable<JUser> {
     return this._http.get<JUser>('/data/user.json');
+  }
+
+  updateIssue(issue: JIssue) {
+    this._store.update((state) => {
+      let issues =arrayUpdate(state.issues, issue.id, issue);
+      console.log(issues);
+      return {
+        ...state,
+        issues
+      };
+    });
   }
 }
