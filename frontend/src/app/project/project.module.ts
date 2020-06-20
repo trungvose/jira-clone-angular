d97b16035bf0c178287b513c5246232656a6e25e
Trung Vo <trung.vo@zyllem.com>
2020-06-20 23:36:53 +0800

    Add the issue modal and issue loader

--- frontend/src/app/project/project.module.ts
+++ frontend/src/app/project/project.module.ts
@@ -2,8 +2,11 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ContentLoaderModule } from '@ngneat/content-loader';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { JiraControlModule } from '../jira-control/jira-control.module';
 import { BoardPageComponents } from './components/board';
 import { NavigationComponents } from './components/navigation';
 import { NZ_JIRA_ICONS } from './config/icons';
@@ -11,15 +14,13 @@ import { BoardComponent } from './pages/board/board.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project.component';
-import { JiraControlModule } from '../jira-control/jira-control.module';
-
-@NgModule({
+ @NgModule({
   declarations: [
     ...NavigationComponents,
     ...BoardPageComponents,
     ProjectComponent,
     BoardComponent,
-    SettingsComponent
+    SettingsComponent,
   ],
   imports: [
     CommonModule,
@@ -28,7 +29,9 @@ import { JiraControlModule } from '../jira-control/jira-control.module';
     DragDropModule,
     NzToolTipModule,
     ReactiveFormsModule,
-    JiraControlModule
+    JiraControlModule,
+    NzModalModule,
+    ContentLoaderModule
   ]
 })
 export class ProjectModule {}
