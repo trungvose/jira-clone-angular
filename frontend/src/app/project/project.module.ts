d5b215a2abf7bcd14d3a83362d7b74bcf895ef74
Trung Vo <trung.vo@zyllem.com>
2020-06-21 23:01:36 +0800

    Update issue type

--- frontend/src/app/project/project.module.ts
+++ frontend/src/app/project/project.module.ts
@@ -1,20 +1,21 @@
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ContentLoaderModule } from '@ngneat/content-loader';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { JiraControlModule } from '../jira-control/jira-control.module';
 import { BoardPageComponents } from './components/board';
+import { IssueUtilComponents } from './components/issues';
 import { NavigationComponents } from './components/navigation';
 import { NZ_JIRA_ICONS } from './config/icons';
 import { BoardComponent } from './pages/board/board.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project.component';
-import { IssueUtilComponents } from './components/issues';
 
  @NgModule({
   declarations: [
@@ -28,12 +29,14 @@ import { IssueUtilComponents } from './components/issues';
   imports: [
     CommonModule,
     ProjectRoutingModule,
-    NzIconModule.forChild(NZ_JIRA_ICONS),
+    NzIconModule.forChild(NZ_JIRA_ICONS),    
+    NzToolTipModule,    
+    NzModalModule,
+    NzSelectModule,
     DragDropModule,
-    NzToolTipModule,
+    FormsModule,
     ReactiveFormsModule,
     JiraControlModule,
-    NzModalModule,
     ContentLoaderModule
   ]
 })
