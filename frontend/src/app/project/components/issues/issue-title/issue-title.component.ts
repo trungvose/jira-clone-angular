e3dc3c7db457f7384677293c8c1e0fdeb0b5e806
Trung Vo <trung.vo@zyllem.com>
2020-06-23 23:47:13 +0800

    Add inline title edit

--- frontend/src/app/project/components/issues/issue-title/issue-title.component.ts
+++ frontend/src/app/project/components/issues/issue-title/issue-title.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { JIssue } from '@trungk18/interface/issue';
+import { FormControl } from '@angular/forms';
+import { ProjectService } from '@trungk18/project/state/project/project.service';
 
 @Component({
   selector: 'issue-title',
   templateUrl: './issue-title.component.html',
   styleUrls: ['./issue-title.component.scss']
 })
-export class IssueTitleComponent implements OnInit {
+export class IssueTitleComponent implements OnChanges {
   @Input() issue: JIssue;
+  titleControl: FormControl;
 
-  constructor() {}
+  constructor(private _projectService: ProjectService) {}
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    let issueChange = changes.issue;
+    if (issueChange.currentValue !== issueChange.previousValue) {
+      this.titleControl = new FormControl(this.issue.title);
+    }
+  }
+
+  onBlur() {
+    this._projectService.updateIssue({
+      ...this.issue,
+      title: this.titleControl.value
+    });
+  }
 }
