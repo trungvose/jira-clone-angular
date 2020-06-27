c542370f2d73fa36dbe3a4bcc2e17e620dd8e38a
Trung Vo <trungk18@gmail.com>
2020-06-27 17:43:51 +0800

    No animation and minor CSS fixed

--- frontend/src/app/project/components/add-issue-modal/issue-assignees-select/issue-assignees-select.component.ts
+++ frontend/src/app/project/components/add-issue-modal/issue-assignees-select/issue-assignees-select.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef, ViewEncapsulation } from '@angular/core';
 import { JUser } from '@trungk18/interface/user';
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'issue-assignees-select',
   templateUrl: './issue-assignees-select.component.html',
-  styleUrls: ['./issue-assignees-select.component.scss']
+  styleUrls: ['./issue-assignees-select.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class IssueAssigneesSelectComponent implements OnInit {
   @Input() control: FormControl;
