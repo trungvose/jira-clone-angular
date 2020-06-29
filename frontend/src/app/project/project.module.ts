171c9a03edda617b50bbcc03b4ecff958758e9ca
Trung Vo <trungk18@gmail.com>
2020-06-29 20:50:27 +0800

    Update README for ng-zorro, add popover for about

--- frontend/src/app/project/project.module.ts
+++ frontend/src/app/project/project.module.ts
@@ -4,19 +4,26 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ContentLoaderModule } from '@ngneat/content-loader';
+import { AutofocusDirective } from '@trungk18/core/directives/autofocus.directive';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 import { QuillModule } from 'ngx-quill';
 import { JiraControlModule } from '../jira-control/jira-control.module';
 import { AddIssueModalComponent } from './components/add-issue-modal/add-issue-modal.component';
+import { IssueAssigneesSelectComponent } from './components/add-issue-modal/issue-assignees-select/issue-assignees-select.component';
+import { IssuePrioritySelectComponent } from './components/add-issue-modal/issue-priority-select/issue-priority-select.component';
+import { IssueReporterSelectComponent } from './components/add-issue-modal/issue-reporter-select/issue-reporter-select.component';
+import { IssueTypeSelectComponent } from './components/add-issue-modal/issue-type-select/issue-type-select.component';
 import { BoardPageComponents } from './components/board';
 import { IssueUtilComponents } from './components/issues';
 import { NavigationComponents } from './components/navigation';
+import { ResizerComponent } from './components/navigation/resizer/resizer.component';
 import { IssueResultComponent } from './components/search/issue-result/issue-result.component';
 import { SearchDrawerComponent } from './components/search/search-drawer/search-drawer.component';
 import { UserComponent } from './components/user/user.component';
@@ -26,12 +33,6 @@ import { FullIssueDetailComponent } from './pages/full-issue-detail/full-issue-d
 import { SettingsComponent } from './pages/settings/settings.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project.component';
-import { IssueTypeSelectComponent } from './components/add-issue-modal/issue-type-select/issue-type-select.component';
-import { IssuePrioritySelectComponent } from './components/add-issue-modal/issue-priority-select/issue-priority-select.component';
-import { IssueReporterSelectComponent } from './components/add-issue-modal/issue-reporter-select/issue-reporter-select.component';
-import { IssueAssigneesSelectComponent } from './components/add-issue-modal/issue-assignees-select/issue-assignees-select.component';
-import { ResizerComponent } from './components/navigation/resizer/resizer.component';
-import { AutofocusDirective } from '@trungk18/core/directives/autofocus.directive';
 
 @NgModule({
   declarations: [
@@ -63,6 +64,7 @@ import { AutofocusDirective } from '@trungk18/core/directives/autofocus.directiv
     NzSelectModule,
     NzNotificationModule,
     NzDrawerModule,
+    NzPopoverModule,
     DragDropModule,
     TextFieldModule,
     FormsModule,
