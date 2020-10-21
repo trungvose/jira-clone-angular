debf2c5a6ef15580f487fc265addbbba68da7718
Trung Vo <trungk18@gmail.com>
2020-10-21 18:42:39 +0800

    Add cypress first test

--- frontend/cypress/integration/create-issue.spec.ts
+++ frontend/cypress/integration/create-issue.spec.ts
@@ -5,6 +5,7 @@ describe('Create Issue', () => {
 
   it('should open create issue modal', () => {
     cy.get("[aria-label='Create issue']").click();
-    cy.get("nz-modal-container .modal-title").should("have.text", "Create issue")
+    cy.get('nz-modal-container .modal-title').should('have.text', 'Create issue');
+    cy.get("nz-modal-container [name='IssueTitle']").type('This is a test issue').should('have.value', 'This is a test issue');
   });
 });
