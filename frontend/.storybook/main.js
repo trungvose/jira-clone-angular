57f310cda299b1a3efba13fa4fa624be4573fad2
Trung Vo <trungk18@gmail.com>
2020-10-26 22:16:54 +0800

    Update introduction

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -5,7 +5,8 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-google-analytics"
   ],
   webpackFinal: async (config, { configType }) => {
     // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
