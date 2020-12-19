ce881e0a66faa01fd9981589cbbc48d92d83b9d6
Trung Vo <trungk18@gmail.com>
2020-12-19 10:12:53 +0800

    Add snow components

---
+++ frontend/src/app/core/snow/snow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'j-snow',
+  templateUrl: './snow.component.html',
+  styleUrls: ['./snow.component.scss']
+})
+export class SnowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
