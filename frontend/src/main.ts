4d2efcdbfaa367d0972eec8b573cee308def36d6
Trung Vo <trungk18@gmail.com>
2020-12-24 12:18:45 +0800

    Remove Sentry comment

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -16,8 +16,6 @@ const initSentry = () => {
       })
     ],
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
     tracesSampleRate: 1.0
   });
 };
