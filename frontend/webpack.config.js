d037c3d03b8d568556240c6fa108c22527177735
Jordan <jordan@jpdesigning.com>
2022-02-28 15:32:20 -0500

    build(frontend): update app to run angular 13

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,24 +4,15 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: [
-          {
-            loader: 'postcss-loader',
-            options: {
-              ident: 'postcss',
-              syntax: 'postcss-scss',
-              plugins: () => [
-                require('postcss-import'),
-                require('tailwindcss'),
-                require('autoprefixer')       
-              ]
-            }
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            ident: 'postcss',
+            syntax: 'postcss-scss',
+            plugins: ['postcss-import', 'tailwindcss', 'autoprefixer'],
           },
-          {
-            loader: 'sass-loader'
-          }
-        ]
-      }
-    ]
-  }
-};
+        },
+      },
+    ],
+  },
+}
