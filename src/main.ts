e7ffa4987188471ec5da5dfb33e29851c10aef40
Trung Vo <trungk18@gmail.com>
2025-06-28 17:36:53 +0800

    chore: update sentry code

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,24 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import * as Sentry from '@sentry/angular';
-import { Integrations } from '@sentry/tracing';
+import { init, browserTracingIntegration } from '@sentry/angular';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// const initSentry = () => {
-//   Sentry.init({
-//     dsn: 'https://b2af8332e38f486d910f06b79df66365@o495789.ingest.sentry.io/5569161',
-//     autoSessionTracking: true,
-//     integrations: [
-//       new Integrations.BrowserTracing({
-//         tracingOrigins: ['localhost', 'https://jira.trungk18.com/'],
-//         routingInstrumentation: Sentry.routingInstrumentation
-//       })
-//     ],
+const initSentry = () => {
+  init({
+    dsn: 'https://b2af8332e38f486d910f06b79df66365@o495789.ingest.sentry.io/5569161',
+    integrations: [
+      browserTracingIntegration(),
+    ],
+    tracePropagationTargets: ['localhost', 'jira.trungk18.com'],
+    tracesSampleRate: 1.0
+  });
+};
 
-//     tracesSampleRate: 1.0
-//   });
-// };
-
-// if (environment.production) {
-//   enableProdMode();
-//   initSentry();
-// }
+if (environment.production) {
+  enableProdMode();
+  initSentry();
+}
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
