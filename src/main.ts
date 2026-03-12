bd330604d0bfcf4449018b7a84b4e83d2514996a
Trung Vo <trungk18@gmail.com>
2026-03-12 19:30:37 +0800

    refactor: remove all remaining NgModules, migrate to provideRouter and route configs

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,19 @@
 import { enableProdMode, ErrorHandler, APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { bootstrapApplication } from '@angular/platform-browser';
 import * as Sentry from '@sentry/angular';
 import { init, browserTracingIntegration } from '@sentry/angular';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { QuillModule } from 'ngx-quill';
+import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
-import { QuillModule } from 'ngx-quill';
-import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
-import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { AppRoutingModule } from './app/app-routing.module';
-import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { Router } from '@angular/router';
-import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
+import { appRoutes } from './app/app.routes';
 
 const initSentry = () => {
   init({
@@ -35,7 +33,15 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, ReactiveFormsModule, AppRoutingModule, NzSpinModule, NzIconModule.forRoot([]), environment.production ? [] : AkitaNgDevtools, AkitaNgRouterStoreModule, QuillModule.forRoot()),
+        provideRouter(appRoutes),
+        provideAnimations(),
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(
+            NzIconModule.forRoot([]),
+            environment.production ? [] : AkitaNgDevtools,
+            AkitaNgRouterStoreModule,
+            QuillModule.forRoot()
+        ),
         {
             provide: NG_ENTITY_SERVICE_CONFIG,
             useValue: { baseUrl: 'https://jsonplaceholder.typicode.com' }
@@ -54,8 +60,6 @@ bootstrapApplication(AppComponent, {
             deps: [Sentry.TraceService],
             multi: true,
         },
-        provideAnimations(),
-        provideHttpClient(withInterceptorsFromDi())
     ]
 })
   .catch((err) => console.error(err));
